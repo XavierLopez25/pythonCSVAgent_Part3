12dcc5ed9a40a494c6ffa569045982ebb2d5405d
XavierLopez25 <thexlopez59@gmail.com>
2024-11-14 01:03:47 -0600

    feat: added full agent functionality on main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+from typing import Any, Dict
+
 from dotenv import load_dotenv
 from langchain import hub
-from langchain_experimental.agents import create_csv_agent
+from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
 
 load_dotenv()
 
 def main():
     print("Start...")
-
-    instructions = """You are an agent designed to write and execute Python code to answer questions.
+    instructions = """You are an agent designed to write and execute python code to answer questions.
     You have access to a python REPL, which you can use to execute python code.
+    You have qrcode package installed.
     If you get an error, debug your code and try again.
-    Only use the output of tour code to answer questions.
-    You might know th answer without running any code, but you should still run the code to get the answer.
+    Only use the output of your code to answer the question.
+    You might know the answer without running any code, but you should still run the code to get the answer.
     If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
     """
 
@@ -22,33 +25,62 @@ def main():
     prompt = base_prompt.partial(instructions=instructions)
 
     tools = [PythonREPLTool()]
-
-    agent = create_react_agent(
+    python_agent = create_react_agent(
         prompt=prompt,
         llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
         tools=tools,
     )
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-    # agent_executor.invoke(
-    #     input={
-    #         "input": """generate and save in current working directory 2 QR codes that point to www.google.com,
-    #         you have qrcode package installed already."""
-    #     }
-    # )
+    python_agent_executor = AgentExecutor(agent=python_agent, tools=tools, verbose=True)
 
-    csv_agent = create_csv_agent(
+    csv_agent_executor: AgentExecutor = create_csv_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4"),
         path="episode_info.csv",
         verbose=True,
         allow_dangerous_code=True,
     )
 
-    csv_agent.invoke(
-        input={
-            "input":  """how many episodes did Larry David write?""" # Esta es una pregunta más simple: """how many columns are there in the file episode_info.csv?"""
-        }
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        return python_agent_executor.invoke({"input": original_prompt})
+
+    tools = [
+        Tool(name="Python Agent", func=python_agent_executor_wrapper, description="""Useful when you need to transform natural language to python and execute the python code, returning the results of the code execution DOES NOT ACCEPT CODE AS INPUT"""),
+        Tool(name="CSV Agent",  func=csv_agent_executor.invoke, description="""Useful when you need to answer questions over episode_info.csv, takes an input the entire question and returns the answer after running pandas calculations"""),
+    ]
+
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools
     )
+
+    grand_agent_executor = AgentExecutor(
+        agent=grand_agent,
+        tools=tools,
+        verbose=True,
+    )
+
+    # Caso para CSV Agent
+    print(
+         grand_agent_executor.invoke(
+             {
+                 "input": "which season has the most episodes?"
+             }
+         )
+     )
+
+    # Caso para Python Code Agent
+    # print(
+    #     grand_agent_executor.invoke(
+    #         {
+    #             "input": "Generate and save in current working directory 5 qrcodes that points to www.google.com, name them as QR_CODE_PYTHON_AGENT_1, QR_CODE_PYTHON_AGENT_2, etc.",
+    #         }
+    #     )
+    # )
 
 if __name__ == "__main__":
     main()
+
+
+
